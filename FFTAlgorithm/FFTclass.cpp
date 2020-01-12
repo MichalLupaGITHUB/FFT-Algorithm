d1c1b463023f939fa628c02551124392fdad6139
MichalLupa <michal.rafal.lupa@gmail.com>
2020-01-12 23:08:45 +0100

    Dodanie projektu Algorytm FFT

---
+++ FFTAlgorithm/FFTclass.cpp
@@ -0,0 +1,78 @@
+﻿#include "pch.h"
+#include "FFTclass.h"
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+// Konstruktor
+FFT::FFT()
+{
+
+}
+
+// Alokacja pamięci
+void FFT::MemoryAllocation()
+{
+	DataInForFFT = (double*)malloc(sizeof(double) * FFT::N);
+	DataOutFromFFT = (fftw_complex *)malloc(sizeof(fftw_complex) * FFT::N);
+	DataInForIFFT = (double *)malloc(sizeof(double) * FFT::N);
+}
+
+// Wypelnienie bufora próbkami
+void FFT::FillDatabySamples()
+{
+	for (int iterator = 0; iterator < FFT::N; iterator++)
+	{
+		DataInForFFT[iterator] = (iterator + 1) / 3;
+	}
+
+}
+
+// Wykoannaie FFT
+void FFT::FunctionMakeFFT()
+{
+	MakeFFT = fftw_plan_dft_r2c_1d(FFT::N, DataInForFFT, DataOutFromFFT, FFTW_ESTIMATE);
+	fftw_execute(MakeFFT);
+}
+
+// Wyniki FFT
+void FFT::DispalyFFTResults()
+{
+	printf("Re: %f Im: %f\n", DataOutFromFFT[0][0], DataOutFromFFT[1][1]);
+}
+
+// Wykonanie programu FFT
+void FFT::FFT_Run()
+{
+	FFT::MemoryAllocation();
+	FFT::FillDatabySamples();
+	FFT::FunctionMakeFFT();
+	FFT::DispalyFFTResults();
+}
+
+// Wykonanie odwrotnej FFT
+void FFT::FunctionMakeIFFT()
+{
+	MakeIFFT = fftw_plan_dft_c2r_1d(N, DataOutFromFFT, DataInForIFFT, FFTW_ESTIMATE);
+	fftw_execute(MakeIFFT);
+}
+
+// Wyniki IFFT
+void FFT::DispalyIFFTResults()
+{
+	printf("Odwrotna FFT: \n");
+
+	for (int iterator = 0; iterator < N; iterator++)
+	{
+		printf("%f\n", DataInForIFFT[iterator] / (double)(N));
+	}
+}
+
+// Wykonanie programu IFFT
+void FFT::IFFT_Run()
+{
+	FFT::FunctionMakeIFFT();
+	FFT::DispalyIFFTResults();
+}
+
---
+++ FFTAlgorithm/FFTclass.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "fftw3.h"
+
+class FFT
+{
+private:
+	double *DataInForFFT; //dane wejściowe dla FFT
+	double *DataInForIFFT;//dane wyjściowe dla IFFT - odwrotna FFT
+	fftw_complex *DataOutFromFFT;//wyjście FFT
+	fftw_plan MakeIFFT;//dla wykonania IFFT - odwrotna FFT
+	fftw_plan MakeFFT;//dla wykonania FFT
+	const int N = 10; // ilość próbek
+
+	// Functions
+	void MemoryAllocation();
+	void FillDatabySamples();
+	void FunctionMakeFFT();
+	void DispalyFFTResults();
+	void FunctionMakeIFFT();
+	void DispalyIFFTResults();
+
+public:
+	FFT();
+	void FFT_Run();
+	void IFFT_Run();
+
+protected:
+
+};
